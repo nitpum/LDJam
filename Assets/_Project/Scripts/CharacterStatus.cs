0c7bceb868488ef67f43eb2f0171501a22ddcc18
nitpum <nitpum@outlook.com>
2020-04-18 22:55:39 +0700

    Add character status

---
+++ Assets/_Project/Scripts/CharacterStatus.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CharacterStatus")]
+public class CharacterStatus : ScriptableObject
+{
+
+    public bool infinityValue;
+    public float maxValue;
+    public float initialValue;
+    public float value;
+
+}
