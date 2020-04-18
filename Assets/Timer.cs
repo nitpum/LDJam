b1775e03487b55008c05f206088fb9fb622ec1c7
Raidsoul <Thunderock01@gmail.com>
2020-04-18 22:57:18 +0700

    Update Timer Ver1

---
+++ Assets/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Update is called once per frame
+    [SerializeField] float timer;
+    [SerializeField] int day;
+    void Start()
+    {
+        timer = 0;
+        day = 0;
+    }
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if(timer > 5f)
+        {
+            day += 1;
+            timer = 0;
+        }
+    }
+}
